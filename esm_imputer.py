8c1e2ab9ea5517a9808c45423c9b64b78d17886a
Ronnie-Crawford <113427947+Ronnie-Crawford@users.noreply.github.com>
2024-10-16 14:20:24 +0100

    Update esm_imputer.py

--- esm_imputer.py
+++ esm_imputer.py
@@ -12,7 +12,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 
 from embeddings import fetch_esm_embeddings_batched, setup_esm
 from helpers import pad_variable_length_sequences
-from model import ESMImputer, setup_models
+from downstream import ESMImputer, setup_model
 
 def sub_seq(seq, position, mut):
         """
