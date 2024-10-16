c59dc0f7666fba7a530f0a0cc2c81a8da2b02799
Ally Dunham <ad44@sanger.ac.uk>
2024-10-16 09:55:12 +0100

    Stub script

---
+++ esm_imputer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Basic denoising diffusion model for imputing missing MAVE data
+"""
+import argparse
+import sys
+
+import numpy as np
+import pandas as pd
+
+def data_loader():
+    pass
+
+class model:
+    pass
+
+def main():
+    pass
+
+# def parse_args(arg_list=None):
+#     """
+#     Construct argument parser
+#     """
+#     parser = argparse.ArgumentParser(description=__doc__,
+#                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+#     parser.add_argument("fastas", nargs="+", metavar="F",
+#                         help="Files to count sequences from")
+
+#     return parser.parse_args(arg_list)
+
+if __name__ == "__main__":
+    main()
