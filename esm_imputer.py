09dfc4f9c8ccbcc8c41eafc4ea74716dc579bce1
Ally Dunham <ad44@sanger.ac.uk>
2024-10-16 11:52:34 +0100

    First pass data loader

--- esm_imputer.py
+++ esm_imputer.py
@@ -14,19 +14,46 @@ from embeddings import setup_esm, fetch_esm_embeddings_batched
 class MAVELoader():
     """MAVE Dataset Loader"""
     def __init__(self):
-        self.sequences = {
+        sequences = {
             i.id.split("|")[1]: str(i.seq) for i in SeqIO.parse("mavedb_uniprot.fa", "fasta")
         }
 
-        self.mavedb = pd.read_csv("mavedb_singles_wide.tsv", sep="\t")
+        mavedb = pd.read_csv("mavedb_singles_wide.tsv", sep="\t")
 
-        df = self.mavedb[self.mavedb.identifier_uniprot.isin(self.sequences.keys())]
+        df = mavedb[
+            mavedb.identifier_uniprot.isin(sequences.keys())
+            ].filter(regex="identifier_uniprot|position|_norm_score$")
+
+        score_df = pd.melt(df, id_vars=["identifier_uniprot", "position"], var_name="mut", value_name="norm_score")
+        score_df.mut = [i.split("_")[0] for i in score_df.mut]
+
+        score_df["seq"] = [self.sub_seq(sequences[u], p, m) for u, p, m in zip(score_df.identifier_uniprot, score_df.position, score_df.mut)]
+
+        data = score_df.merge(df, on = ["identifier_uniprot", "position"], how = "left").dropna(subset="mut")
+
+        self.score = data.norm_score.to_numpy()
+        self.position = data.position.to_numpy()
+        self.seq = data.seq.to_numpy()
+        self.score_matrix = data.filter(regex="_norm_score$").to_numpy()
+        self.score_matrix = np.nan_to_num(self.score_matrix, 0, copy=False)
+
+    def sub_seq(seq, position, mut):
+        """
+        Add a substitution to a sequence
+        """
+        mut = mut if not mut == "stop" else "*"
+        return "".join([aa if not i == position else mut for i, aa in enumerate(seq)])
 
     def __len__(self):
-        pass
+        return len(self.score)
 
     def __getitem__(self, index):
-        pass
+        return {
+            "score": self.score[index],
+            "position": self.position[index],
+            "seq": self.seq[index],
+            "score_vector": self.score_matrix[index,]
+        }
 
 class model:
     pass
